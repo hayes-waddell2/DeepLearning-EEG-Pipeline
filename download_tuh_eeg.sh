019c45d6fc4c8085ce202b7d16e971dc436b64d9
Hayes Waddell (RIT Student) <hw2693@sporcsubmit.rc.rit.edu>
2026-03-24 18:59:10 -0400

    load rsync via spack for testing on debug partition

--- download_tuh_eeg.sh
+++ download_tuh_eeg.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/bin/bash -1
 #SBATCH --job-name=tuh-eeg-download
 #SBATCH --account=eeg-cnn-lstm
-#SBATCH --partition=sporc-cpu
+#SBATCH --partition=debug
 #SBATCH --output=/shared/rc/eeg-cnn-lstm/download_%j.log
-#SBATCH --time=24:00:00
+#SBATCH --time=0-1:00:00
+#SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=4G
 
-which rsync
+spack load rsynch@3.2.7
+
 rsync -auvxL -e "ssh -i ~/.ssh/id_ed25519_tuh" \
     nedc-tuh-eeg@www.isip.piconepress.com:data/tuh_eeg/tuh_eeg_abnormal/v3.0.1 \
     /shared/rc/eeg-cnn-lstm/data/
