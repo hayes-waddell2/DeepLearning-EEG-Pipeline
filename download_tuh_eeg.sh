c506ef1ac9a140241e859e5d169f99acb191718e
Hayes Waddell (RIT Student) <hw2693@sporcsubmit.rc.rit.edu>
2026-03-24 19:09:54 -0400

    switch to sporc-cpu for full download and fix spack typo

--- download_tuh_eeg.sh
+++ download_tuh_eeg.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -l
 #SBATCH --job-name=tuh-eeg-download
 #SBATCH --account=eeg-cnn-lstm
-#SBATCH --partition=debug
+#SBATCH --partition=sporc-cpu
 #SBATCH --output=/shared/rc/eeg-cnn-lstm/download_%j.log
-#SBATCH --time=0-1:00:00
+#SBATCH --time=5-0:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=4G
 
 . /tools/spack/share/spack/setup-env.sh
-spack load rsynch@3.2.7
+spack load rsync@3.2.7
 
 rsync -auvxL -e "ssh -i ~/.ssh/id_ed25519_tuh" \
     nedc-tuh-eeg@www.isip.piconepress.com:data/tuh_eeg/tuh_eeg_abnormal/v3.0.1 \
