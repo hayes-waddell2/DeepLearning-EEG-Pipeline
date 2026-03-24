c7cd18d0fd41723baa7fd87f170d077e63e1da0e
Hayes Waddell (RIT Student) <hw2693@sporcsubmit.rc.rit.edu>
2026-03-24 18:38:42 -0400

    add slurm download job for tuh eeg abnormal dataset

---
+++ download_tuh_eeg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=tuh-eeg-download
+#SBATCH --account=eeg-cnn-lstm
+#SBATCH --output=/shared/rc/eeg-cnn-lstm/download_%j.log
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=4G
+
+rsync -auvxL -e "ssh -i ~/.ssh/id_ed25519_tuh" \
+    nedc-tuh-eeg@www.isip.piconepress.com:data/tuh_eeg/tuh_eeg_abnormal/v3.0.1 \
+    /shared/rc/eeg-cnn-lstm/data/
