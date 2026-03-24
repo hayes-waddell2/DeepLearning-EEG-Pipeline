add8ad98eab87cc7ea8903ad9ba7837fba8f8673
Hayes Waddell (RIT Student) <hw2693@sporcsubmit.rc.rit.edu>
2026-03-24 18:46:38 -0400

    use full path for rsync on compute node

--- download_tuh_eeg.sh
+++ download_tuh_eeg.sh
@@ -8,6 +8,8 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=4G
 
-rsync -auvxL -e "ssh -i ~/.ssh/id_ed25519_tuh" \
+
+
+/usr/bin/rsync -auvxL -e "ssh -i ~/.ssh/id_ed25519_tuh" \
     nedc-tuh-eeg@www.isip.piconepress.com:data/tuh_eeg/tuh_eeg_abnormal/v3.0.1 \
     /shared/rc/eeg-cnn-lstm/data/
